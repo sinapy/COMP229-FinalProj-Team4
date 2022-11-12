3b5450d999bb26d76b25d95e12772c4f25425d53
KanikaAggarwal19 <114032110+KanikaAggarwal19@users.noreply.github.com>
2022-11-13 02:45:50 +0530

    Added new filed expire On and status

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,6 @@
 // create a reference to the model
 let productModel = require('../models/product');
+let moment = require('moment');
 
 // Gets all posts from the Database and renders the page to list them all.
 module.exports.postsList = function(req, res, next) {  
@@ -57,7 +58,8 @@ module.exports.displayEditPage = (req, res, next) => {
         {
             //show the edit view
             res.render('products/create-post', {
-                post: postToShow
+                post: postToShow,
+				moment: moment
             })
         }
     });  
@@ -142,6 +144,8 @@ module.exports.processAddPage = (req, res, next) => {
         _id: Math.random().toString(36).slice(2),
         title: req.body.title,
         price: req.body.price,
+ 		status: req.body.status,
+ 		expires_on: req.body.expires_on,
         
     });
 

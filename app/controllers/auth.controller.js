d5427a3e0de8fde1266979179574bda4351eedf8
sinapy <sinasarsharpour@cmail.carleton.ca>
2022-12-03 23:09:38 -0500

    Update #17

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -104,6 +104,7 @@ exports.signin = (req, res) => {
                 username: user.username,
                 email: user.email,
                 roles: authorities,
+                accessToken: token
             });
         });
 };
--- app/routes/posts.routes.js
+++ app/routes/posts.routes.js
@@ -31,12 +31,12 @@ module.exports = function(app) {
     app.get('/api/posts/:id', controller.getPostDetailsById);
 
 // createnew post
-    app.post('/api/posts/', controller.createPost);
+    app.post('/api/posts/', [authJwt.verifyToken], controller.createPost);
 
 // update a post
-    app.put('/api/posts/:id', controller.updatePost);
+    app.put('/api/posts/:id', [authJwt.verifyToken], controller.updatePost);
 
 // delete a post
-    app.delete('/api/posts/:id', controller.deletePost);
+    app.delete('/api/posts/:id', [authJwt.verifyToken], controller.deletePost);
 
 }

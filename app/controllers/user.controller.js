812fa733f71e7298e13dce6aaf0dc655b96f0527
sinapy <sinasarsharpour@cmail.carleton.ca>
2022-12-11 21:10:11 -0500

    Update #20

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,9 +1,23 @@
+const User = require("../models/user.model");
 exports.allAccess = (req, res) => {
     res.status (200).send("Public Content.");
 };
 exports.userBoard = (req, res) => {
     res.status(200).send("User Content.");
 };
+exports.getName = (req, res) => {
+    User.findById(req.userId).exec((err, user) => {
+        if (err) {
+            res.status(501).send({message: err});
+            return;
+        }
+        let username = user.username()
+        if (username == null) {
+            username = ""
+        }
+        res.status(200).send(user.username())
+    });
+}
 exports.adminBoard = (req, res) => {
     res.status(200).send("Admin Content.");
 };
